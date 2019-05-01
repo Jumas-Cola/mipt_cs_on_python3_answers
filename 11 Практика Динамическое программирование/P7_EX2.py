c09810d9b37d3a4d350795ec10f8a8c2d66cd02b
Jumas <kbbyfl91@gmail.com>
2019-05-01 17:46:42 +0300

    upd

---
+++ 11 Практика Динамическое программирование/P7_EX2.py
@@ -0,0 +1,11 @@
+def kuz(n):
+    F = [-1]*(n+1)
+    F[0] = 0
+    F[1] = 1
+    for i in range(2, n+1):
+        F[i] = F[i - 1] + F[i - 2]
+        if i % 3 == 0:
+            F[i] += F[i // 3]
+    return F[n]
+
+print(kuz(6))
