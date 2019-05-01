430119747881f86c3ae7022ff03f35633544f306
Jumas <kbbyfl91@gmail.com>
2019-05-01 20:32:14 +0300

    upd

--- 11 Практика Динамическое программирование/P7_EX4.py
+++ 11 Практика Динамическое программирование/P7_EX4.py
@@ -1,10 +1,20 @@
 def calculate_min_cost(n, price):
 	C = [0]*(n + 1)
+	prev = [0]*(n + 1)
 	C[0] = price[0]
 	C[1] = price[1] + price[0]
 	for i in range (2, n+1):
 		C[i] = min(C[i-1], C[i-2]) + price[i]
-	return C[n]
+		if C[i-1] < C[i-2]:
+			prev[i] = i - 1
+		else:
+			prev[i] = i - 2
+	path = [n]
+	i = n
+	while i > 0:
+		path.append(prev[i])
+		i = prev[i]
+	return path[::-1]
 
 
 
