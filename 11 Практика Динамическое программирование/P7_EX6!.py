b71fcdf36d0e2b16e3317495defc221281002d3f
Jumas <kbbyfl91@gmail.com>
2019-05-02 21:59:20 +0300

    upd

--- 11 Практика Динамическое программирование/P7_EX6!.py
+++ 11 Практика Динамическое программирование/P7_EX6!.py
@@ -2,15 +2,10 @@ def upd_cell():
     for i in range(N-1,-1,-1):
         for j in range(M-1,-1,-1):
             if cell[i][j] == '-':
-                for k in range(i,0,-1):
-                    cell[k-1][j] = '+'
-                for k in range(j,0,-1):
-                    cell[i][k-1] = '+'
-                while i and j:
-                    cell[i-1][j-1] = '+'
-                    i -= 1
-                    j -= 1
-            if cell[i][j] == ' ' and i < N-1 and j < M-1 and cell[i+1][j] == cell[i][j+1] == cell[i+1][j+1] == '+':
+                if i>0: cell[i-1][j] = '+'
+                if j>0: cell[i][j-1] = '+'
+                if i>0 and j>0: cell[i-1][j-1] = '+'
+            if cell[i][j] == ' ' and i < N-1 and j < M-1 and (cell[i+1][j] == '+' or i+1 > N-1) and (cell[i][j+1] == '+' or j+1 > M-1) and (cell[i+1][j+1] == '+' or (i+1 > N-1 and j+1 > M-1)):
                 cell[i][j] = '-'
 
 M = 8
