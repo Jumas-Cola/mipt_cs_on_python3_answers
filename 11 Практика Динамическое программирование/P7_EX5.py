45c898d025345e02df5b5ff65974120d2cf6eda9
Jumas <kbbyfl91@gmail.com>
2019-05-02 21:18:10 +0300

    added task 5

---
+++ 11 Практика Динамическое программирование/P7_EX5.py
@@ -0,0 +1,30 @@
+def upd_cell():
+    for i in range(N-1,-1,-1):
+        for j in range(M-1,-1,-1):
+            if cell[i][j] == '-':
+                for k in range(i,0,-1):
+                    cell[k-1][j] = '+'
+                for k in range(j,0,-1):
+                    cell[i][k-1] = '+'
+                while i and j:
+                    cell[i-1][j-1] = '+'
+                    i -= 1
+                    j -= 1
+            if cell[i][j] == ' ' and i < N-1 and j < M-1 and cell[i+1][j] == cell[i][j+1] == cell[i+1][j+1] == '+':
+                cell[i][j] = '-'
+
+M = 8
+N = 6
+cell = [[' ' for j in range(M)] for i in range(N)]
+
+cell[N-1][M-1] = '-'
+
+while 1:
+    for i in cell:
+        if ' ' in i:
+            upd_cell()
+            continue
+    break
+
+for i in cell:
+    print(i)
